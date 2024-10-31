0b4e1f893c6a39e22bf1f2d6a80817439104118d
olijouve <17448560+olijouve@users.noreply.github.com>
2024-10-31 11:24:38 +0100

    Update bundle.js

--- browser/bundle.js
+++ browser/bundle.js
@@ -1430,7 +1430,7 @@ module.exports = function(arr, fn, initial){
      * The authentication methods to be included for all API calls.
      * @type {Array.<String>}
      */
-    this.authentications = [{basic:{
+    this.authentications = {basic:{
       type:'basic',
       username:'demat',
       password:'d3m@7'
